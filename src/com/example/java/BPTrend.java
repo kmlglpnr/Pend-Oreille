becb25cf245dddf3390435c96523450e43bebe62
Kemal Gulpinar <kemalgulpnar@gmail.com>
2019-10-21 13:00:27 -0400

    Parsing the datas from given dataset and adding in memory collection storage.

--- src/com/example/java/BPTrend.java
+++ src/com/example/java/BPTrend.java
@@ -3,6 +3,7 @@ package com.example.java;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -50,5 +51,79 @@ public class BPTrend {
         }
     }
 
+    // slope calculation
+    public String doSlopeCalculation(String from, String to){
+        DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+        Date d1 = null;
+        Date d2 = null;
+        try{
+            d1 = format.parse(from);
+            d2 = format.parse(to);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        String result  = "From "
+                + format.format(d1)
+                + " To "
+                + format.format(d2);
+
+        WeatherEntry y1 = null;
+        WeatherEntry y2 = null;
+        int idx = 0, x1 = 0, x2 = 0;
+
+        for(WeatherEntry entry : collectedData){
+            // if d1 is same or before than entry.when
+            if( (y1 == null) && (entry.when.compareTo(d1) >= 0)){
+                y1 = entry;
+                x1 = idx;
+            }
+            // if d2 is same or before than entry.when
+            if (entry.when.compareTo(d2) >= 0){
+                x2 = idx;
+                y2 = entry;
+                break;
+            }
+            idx++;
+        }
+        // formula, slope varible
+        float slope = (y2.pressure - y1.pressure) / (x2 - x1);
+
+        result = result + " The barometric pressure slope is "
+                + String.format("%.6f",slope) + " \nthe foreccast is: ";
+
+        // Trend analysis
+        if(slope < 0 ) {
+            result = result + "inclement weather is closing in \n";
+        } else if(slope == 0){
+            result = result + "current conditions are likely to persist.\n";
+        } else {
+            result  = result + "conditions are improving.\n";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        BPTrend calcTrend = new BPTrend();
+        System.out.println("Reading data...");
+        calcTrend.readData("files/Environmental_Data_Deep_Moor_2012.txt");
+        calcTrend.readData("files/Environmental_Data_Deep_Moor_2013.txt");
+        calcTrend.readData("files/Environmental_Data_Deep_Moor_2014.txt");
+        calcTrend.readData("files/Environmental_Data_Deep_Moor_2015.txt");
+        System.out.println("Done!");
+        System.out.println("Total number of weather data entries: "
+        + calcTrend.collectedData.size());
+
+        String from = "";
+        String to = "";
+
+        System.out.println("Test Case #1: ");
+        from = "2012/01/01 00:30:00";
+        to = "2012/01/01 02:30:00";
+
+        System.out.println(calcTrend.doSlopeCalculation(from, to));
+
+
+    }
 
 }
